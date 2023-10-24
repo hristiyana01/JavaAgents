61284eb6bb85bb498a04f6e99631f6bea8a91d34
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-10-24 07:02:31 +0300

    adding the first created Java agent /from exercise1/

---
+++ src/hello/Agent001.java
@@ -0,0 +1,22 @@
+package hello;
+
+import jade.core.Agent;
+import jade.core.Location;
+
+public class Agent001 extends Agent {
+    protected void setup() {
+        Location location = here();
+        System.out.println("Agent Started: Hello World!");
+        System.out.println("-----About Me:-----");
+        System.out.println("My local name is: " + getLocalName());
+        System.out.println("My globally unique name is: " + getName());
+        System.out.println("-----About Here:-----");
+        System.out.println("I am running in a location called:" + location.getAddress());
+        System.out.println("Which is identified uniquely as: " + location.getID());
+        System.out.println("And is contactable at: " + location.getAddress());
+        System.out.println("Using the protocol: " + location.getProtocol());
+
+        //make this agent terminate
+        //doDelete();
+    }
+}
