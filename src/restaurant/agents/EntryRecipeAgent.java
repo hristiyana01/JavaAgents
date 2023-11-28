9be8e0a2ee71bf2326b903d8a6eeb73331cf8742
hristiyana <96460833+hristiyana01@users.noreply.github.com>
2023-11-28 14:40:29 +0200

    with sending message

--- src/restaurant/agents/EntryRecipeAgent.java
+++ src/restaurant/agents/EntryRecipeAgent.java
@@ -1,6 +1,8 @@
 package restaurant.agents;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
@@ -11,6 +13,8 @@ public class EntryRecipeAgent extends Agent {
     protected void setup() {
 
         Scanner scan = new Scanner(System.in);
+        ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+
 
         System.out.println("Recipe: ");
 
@@ -22,6 +26,7 @@ public class EntryRecipeAgent extends Agent {
 
         Object[] arg = {mealName};
 
+        //sending recipe with message
         try {
             AgentController a = controller.createNewAgent("Cooker", "restaurant.agents.MealAgent", arg);
 
@@ -29,7 +34,15 @@ public class EntryRecipeAgent extends Agent {
         } catch (StaleProxyException e) {
             e.printStackTrace();
         }
-
+        sendMessage();
         doDelete();
+    }
+    private void sendMessage() {
+        AID r = new AID("Peter", AID.ISLOCALNAME);
+        ACLMessage aclMessage = new
+                ACLMessage(ACLMessage.REQUEST);
+        aclMessage.addReceiver(r);
+        aclMessage.setContent("Hello! How are you?");
+        this.send(aclMessage);
     }
 }
